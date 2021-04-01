52d8b6a9e1ea0fce3ea5daec013b0c8ff649ea9e
Manish Raj <Manish.Raj@programmers.io>
2021-04-01 17:54:17 +0530

    Add project files.

---
+++ ApiAuthAuthorization/Controllers/UsersController.cs
@@ -0,0 +1,60 @@
+﻿using ApiAuthAuthorization.Entities;
+using ApiAuthAuthorization.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAuthAuthorization.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : Controller
+    {
+        private IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenicate([FromBody]User userParam)
+        {
+            var user = _userService.Authenticate(userParam.UserName, userParam.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "UserName or Password is Incorrect." });
+
+            return Ok(user);
+        }
+
+        [Authorize(Roles = Role.Admin)]
+        [HttpGet]
+        [Route("getall")]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            return Ok(users);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var user = _userService.GetById(id);
+
+            if(user == null)
+            {
+                return NotFound();
+            }
+            //only allow admin to access other user records
+            var currentUserId = int.Parse(User.Identity.Name);
+            if(id != currentUserId && !User.IsInRole(Role.Admin))
+            {
+                return Forbid();
+            }
+
+            return Ok(user);
+        }
+    }
+}
---
+++ ApiAuthAuthorization/Services/UserService.cs
@@ -0,0 +1,77 @@
+﻿using ApiAuthAuthorization.Entities;
+using ApiAuthAuthorization.Helpers;
+using ApiAuthAuthorization.Repository;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiAuthAuthorization.Services
+{
+    public class UserService : IUserService
+    {
+        private List<User> _users = new List<User>
+        {
+            new User{Id = 1, FirstName = "Admin", LastName = "User", UserName = "admin", Password = "admin", Role = Role.Admin},
+            new User{Id = 2, FirstName = "Normal", LastName = "User", UserName = "user", Password = "user", Role = Role.User}
+        };
+
+        private readonly AppSettings _appSettings;
+
+        public UserService(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+
+        public User Authenticate(string username, string password)
+        {
+            var user = _users.SingleOrDefault(x => x.UserName == username && x.Password == password);
+
+            if (user == null)
+                return null;
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+
+            user.Password = null;
+
+            return user;
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _users.Select(x =>
+            {
+                x.Password = null;
+                return x;
+            });
+        }
+
+        public User GetById(int id)
+        {
+            var user = _users.FirstOrDefault(x => x.Id == id);
+
+            if (user != null)
+                user.Password = null;
+
+            return user;
+        }
+    }
+}
